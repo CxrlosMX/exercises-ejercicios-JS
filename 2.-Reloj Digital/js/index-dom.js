6f5b7de505d0249a6c06f5bba5d6e19773c799a0
CxrlosMX <LuisCRendon131@gmail.com>
2022-01-03 16:28:33 -0600

    Alarma Sonora app

--- 2.-Reloj Digital/js/index-dom.js
+++ 2.-Reloj Digital/js/index-dom.js
@@ -1,8 +1,8 @@
 import { digitalClock, alarm } from "./reloj-digital.js";
 
-const d=document;
+const d = document;
 
-d.addEventListener("DOMContentLoaded",(e)=>{
-    digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
-    alarm("#activar-alarma","#desactivar-alarma");
+d.addEventListener("DOMContentLoaded", (e) => {
+    digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
+    alarm("/assets/008522530_prev.mp3", "#activar-alarma", "#desactivar-alarma");
 });
--- 2.-Reloj Digital/js/reloj-digital.js
+++ 2.-Reloj Digital/js/reloj-digital.js
@@ -20,6 +20,31 @@ export function digitalClock(clock, playBtn, stopBtn) {
         }
     });
 }
-export function alarm(playBtn, stopBtn) {
-
+export function alarm(sound, playBtn, stopBtn) {
+    d.querySelector(stopBtn).disabled = true;
+    let alarmTime;
+    // Creamos una etiqueta audio
+    const $alarm = d.createElement("audio");
+    // Ingresamos dinamicamente al su atributo src
+    $alarm.src = sound;
+    d.addEventListener("click", (e) => {
+        if (e.target.matches(playBtn)) {
+            // El timeout() solo se reproduce una sola vez
+            alarmTime = setTimeout(() => {
+                // Ejecutamos el audio
+                $alarm.play();
+            }, 1000);
+            e.target.disabled = true;
+            d.querySelector(stopBtn).disabled = false;
+        }
+        if (e.target.matches(stopBtn)) {
+            clearTimeout(alarmTime);
+            // Puasamos el sonido 
+            $alarm.pause();
+            //Reiniciamos el audio
+            $alarm.currentTime = 0;
+            d.querySelector(stopBtn).disabled = true;
+            d.querySelector(playBtn).disabled = false;
+        }
+    });
 }
