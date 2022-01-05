31110e7f24d8a48f6ba7bec1729dcdae2c1899c5
CxrlosMX <LuisCRendon131@gmail.com>
2022-01-05 15:59:52 -0600

    local Storage

--- 7.-Dark Mode/js/dark-mode.js
+++ 7.-Dark Mode/js/dark-mode.js
@@ -1,30 +1,62 @@
 const d = document,
-    w = window;
+    w = window,
+    ls = localStorage;
 
 export default function darkMode(btn, classDark) {
     const $themeBtn = d.querySelector(btn),
         $selectors = d.querySelectorAll("[data-dark]");
-        console.log($selectors);
+    console.log($selectors);
     // $selectors nos retorna un nodo con todos los elementos que tienen el data atribute, como lo aplicamos al HTML todos los elementos dentro del HTML tendran esa Data
     let moon = "🌙",
         sun = "☀️";
-    
+
+    const lightMode = () => {
+        $selectors.forEach((el) => el.classList.remove(classDark));
+        $themeBtn.textContent = moon;
+        ls.setItem("theme","light");
+    };
+    const darkMode = () => {
+        // Agregamos la clase
+        $selectors.forEach((el) => el.classList.add(classDark));
+
+        // Y cambiamo el simbolo que esta dentro de nuestro boton
+        $themeBtn.textContent = sun;
+        ls.setItem("theme","dark");
+    };
+
     d.addEventListener("click", (e) => {
         if (e.target.matches(btn)) {
             console.log($themeBtn.textContent);
             //Verificaremos si el boton del tema lo que tiene adentro es una luna
             if ($themeBtn.textContent === moon) {
-                // Agregamos la clase
-                $selectors.forEach((el) => el.classList.add(classDark));
-
-                // Y cambiamo el simbolo que esta dentro de nuestro boton
-                $themeBtn.textContent = sun;
+                darkMode();
             }
             else {
-                $selectors.forEach((el) => el.classList.remove(classDark));
-                $themeBtn.textContent = moon;
+                lightMode();
             }
         }
+    });
+    // Nota: No podemos tener una funcion del mismo tipo que la funcion padre
+    // LocalStorage
+    d.addEventListener("DOMContentLoaded", (e) => {
+        // Este método nos sirvira para verificar si tenemos algo en nuestro local storage
+        // alert("Hola");
+        // console.log(ls.getItem("theme"));
+        if (ls.getItem("theme") === null) {//Si no existe ninguna variable con ese nombre en el localStorage
+
+            // Si no existe ninguna variable entonces creamos una variable
+            ls.setItem("theme", "light")
+
+        }
+        if (ls.getItem("theme")==="light") {
+            lightMode();
+        }
+        if (ls.getItem("theme")==="dark") {
+            darkMode();
+        }
+
+
+
     });
 
 }
--- 7.-Dark Mode/js/index-dom.js
+++ 7.-Dark Mode/js/index-dom.js
@@ -1,6 +1,7 @@
 import darkMode from "./dark-mode.js";
 const d = document;
 
-d.addEventListener("DOMContentLoaded",(e)=>{
-   darkMode(".dark-theme-btn","dark-mode");
+d.addEventListener("DOMContentLoaded", (e) => {
+
 });
+darkMode(".dark-theme-btn", "dark-mode");
