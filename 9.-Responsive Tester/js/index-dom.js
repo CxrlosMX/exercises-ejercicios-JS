d4b1c51bbff88b31647a0ccd8857026ab90e3299
CxrlosMX <LuisCRendon131@gmail.com>
2022-01-05 17:31:10 -0600

    Responsive tester JS

---
+++ 9.-Responsive Tester/js/index-dom.js
@@ -0,0 +1,7 @@
+import responsiveTester from "./responsive-tester.js";
+const d = document;
+
+d.addEventListener("DOMContentLoaded", (e) => {
+    responsiveTester("responsive-tester");
+});
+
---
+++ 9.-Responsive Tester/js/responsive-tester.js
@@ -0,0 +1,20 @@
+const d = document,
+    w = window;
+
+export default function responsiveTester(form) {
+    const $form = d.getElementById(form);
+
+    let tester;
+
+    d.addEventListener("submit",(e)=>{
+        if (e.target===$form) {
+            e.preventDefault();
+            tester=w.open($form.direccion.value,"tester",`innerWidth=${$form.ancho.value},innerHeight=${$form.alto.value}`);
+        }
+    });
+    d.addEventListener("click",(e)=>{
+        if(e.target===$form.cerrar){
+            tester.close();
+        }
+    });
+}
